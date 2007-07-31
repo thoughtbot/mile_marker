e3396f2bcaf857ec8fabc67e1c979327156d93f5
cpytel <cpytel@7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa>
2007-07-31 16:25:16 +0000

    helper name change
    git-svn-id: https://svn.thoughtbot.com/plugins/mile_marker/trunk@152 7bbfaf0e-4d1d-0410-9690-a8bb5f8ef2aa

--- lib/mile_marker.rb
+++ lib/mile_marker.rb
@@ -1,8 +1,8 @@
 module Thoughtbot
   module MileMarkerHelper
-    def todo(detail="")
+    def mile(detail="")
       return unless MileMarker.enabled?
-      "todo=\"" + (detail.is_a?(Fixnum) ? "Milestone " : "") + "#{detail}\""
+      "mile=\"" + (detail.is_a?(Fixnum) ? "Milestone " : "") + "#{detail}\""
     end
   
     def initialize_mile_marker(request = nil)
@@ -18,12 +18,12 @@ module Thoughtbot
   end
     
   class MileMarker  
-    # The environments in which to enable the Todo functionality to run.  Defaults
+    # The environments in which to enable the Mile Marker functionality to run.  Defaults
     # to 'development' only.
     @@environments = ['development']
     cattr_accessor :environments
 
-    # Return true if the Todo functionality is enabled for the current environment
+    # Return true if the Mile Marker functionality is enabled for the current environment
     def self.enabled?
       environments.include?(ENV['RAILS_ENV'])
     end
@@ -32,15 +32,15 @@ module Thoughtbot
       %Q~
 <script type="text/javascript">
 //<![CDATA[
-  function init_todo() {
-    $$('*[todo]').each(function(block, index) {
-      html = '<div id="todo_'+index+'" style="display: none; z-index: 1000; position: absolute; background-color: #000; opacity: .33; filter: alpha(opacity=33); color: #fff; font-family: Verdana; font-weight: bold; font-size: 20px;"><div style="margin: 5px;">'+block.getAttribute('todo')+'</div></div>'
+  function init_miles() {
+    $$('*[mile]').each(function(block, index) {
+      html = '<div id="mile_'+index+'" style="display: none; z-index: 1000; position: absolute; background-color: #000; opacity: .33; filter: alpha(opacity=33); color: #fff; font-family: Verdana; font-weight: bold; font-size: 20px;"><div style="margin: 5px;">'+block.getAttribute('mile')+'</div></div>'
       new Insertion.Before($(block), html);
-      Position.clone($(block), $('todo_'+index));
-      $('todo_'+index).toggle();
+      Position.clone($(block), $('mile_'+index));
+      $('mile_'+index).toggle();
     });
   }
-  Event.observe(window, 'load', init_todo, false);
+  Event.observe(window, 'load', init_miles, false);
 //]]>
 </script>
 ~
--- test/mile_marker_test.rb
+++ test/mile_marker_test.rb
@@ -3,45 +3,45 @@ require File.expand_path(File.join(File.dirname(__FILE__), 'test_helper'))
 class MileMarkerTest < Test::Unit::TestCase
   include Thoughtbot::MileMarkerHelper
 
-  def test_todo_helper_should_return_nothing_if_no_enabled
+  def test_mile_helper_should_return_nothing_if_no_enabled
     Thoughtbot::MileMarker.environments = ['development']
     ENV['RAILS_ENV']="test_environment"
-    output = todo("Milestone 1")
+    output = mile("Milestone 1")
     assert_nil output
   end
 
-  def test_todo_helper_should_include_detail_when_supplied_detail
+  def test_mile_helper_should_include_detail_when_supplied_detail
     Thoughtbot::MileMarker.environments = ['development']
     ENV['RAILS_ENV']="development"
-    output = todo("Milestone 1")
-    assert_equal "todo=\"Milestone 1\"", output
+    output = mile("Milestone 1")
+    assert_equal "mile=\"Milestone 1\"", output
   end
   
-  def test_todo_helper_should_include_add_milestone_when_supplied_integer
+  def test_mile_helper_should_include_add_milestone_when_supplied_integer
     Thoughtbot::MileMarker.environments = ['development']
     ENV['RAILS_ENV']="development"
-    output = todo(1)
-    assert_equal "todo=\"Milestone 1\"", output
+    output = mile(1)
+    assert_equal "mile=\"Milestone 1\"", output
   end
     
-  def test_todo_helper_should_include_no_detail_when_supplied_no_detail
+  def test_mile_helper_should_include_no_detail_when_supplied_no_detail
     Thoughtbot::MileMarker.environments = ['development']
     ENV['RAILS_ENV']="development"
-    output = todo
-    assert_equal "todo=\"\"", output
+    output = mile
+    assert_equal "mile=\"\"", output
   end
   
-  def test_initialize_todo_should_return_nothing_if_not_enabled
+  def test_initialize_mile_should_return_nothing_if_not_enabled
     ENV['RAILS_ENV']="test_environment"
     output = initialize_mile_marker
     assert_nil output
   end
 
-  def test_initialize_todo_should_return_javascript_if_enabled
+  def test_initialize_mile_should_return_javascript_if_enabled
     Thoughtbot::MileMarker.environments = ['development']
     ENV['RAILS_ENV']="development"
     output = initialize_mile_marker
-    assert_match /function init_todo/, output
+    assert_match /function init_miles/, output
   end
   
   def test_javascript_should_be_added_to_head_if_enabled
